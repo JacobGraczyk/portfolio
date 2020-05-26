be24b0341e2b84d642605022fc818245c377fa8d
Jacob Graczyk <jacobgraczyk@Jacobs-MacBook-Pro.local>
2020-05-26 16:51:22 -0400

    Updates

---
+++ precache-manifest.a21bb5e30a7c6800dd125defc5dfe1cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd66cd3b09bfb557542a46c02d7d5eea",
+    "url": "/tangorito/index.html"
+  },
+  {
+    "revision": "6f61e9bdc9746c6e1df8",
+    "url": "/tangorito/static/css/main.bfb5ecd0.chunk.css"
+  },
+  {
+    "revision": "9ff0edb2cdcefe8cc170",
+    "url": "/tangorito/static/js/2.44fad725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tangorito/static/js/2.44fad725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f61e9bdc9746c6e1df8",
+    "url": "/tangorito/static/js/main.52ad8a76.chunk.js"
+  },
+  {
+    "revision": "7229c55fe41497b5ae39",
+    "url": "/tangorito/static/js/runtime-main.f73b8c74.js"
+  },
+  {
+    "revision": "bda048a0f528830af66a7dfbc85bc3eb",
+    "url": "/tangorito/static/media/hockey-faceoff.bda048a0.jpg"
+  },
+  {
+    "revision": "3d01cf54d42f10693b2209f8dd445a19",
+    "url": "/tangorito/static/media/jango.3d01cf54.jpg"
+  },
+  {
+    "revision": "d8d567bb361230c2e02d3236dcbb3178",
+    "url": "/tangorito/static/media/logo.d8d567bb.svg"
+  },
+  {
+    "revision": "81a01f49227bb073f4cf1acecb8d4f26",
+    "url": "/tangorito/static/media/tablelinensforless.81a01f49.jpg"
+  },
+  {
+    "revision": "025db4e451b98cbaf2a0a8e27906adae",
+    "url": "/tangorito/static/media/tangorito.025db4e4.jpg"
+  }
+]);
